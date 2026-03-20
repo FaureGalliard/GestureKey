acea0d6786bb35b0850bf64a58d990af1f69a0e6
FaureGalliard <angelcrispinvaldivia@gmail.com>
2026-03-20 16:43:40 -0500

    feat first release

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,56 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
+
+struct ServerProcess(std::sync::Mutex<Option<std::process::Child>>);
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(ServerProcess(std::sync::Mutex::new(None)))
+        .setup(|app| {
+            let server_exe = if cfg!(target_os = "windows") {
+                "server.exe"
+            } else {
+                "server"
+            };
+
+            let server_path = if cfg!(debug_assertions) {
+                std::env::current_dir()
+                    .unwrap()
+                    .parent()
+                    .unwrap()
+                    .join("src-tauri")
+                    .join("server")
+                    .join(server_exe)
+            } else {
+                std::env::current_exe()
+                    .unwrap()
+                    .parent()
+                    .unwrap()
+                    .join("server")
+                    .join(server_exe)
+            };
+
+            println!("[SERVER] path: {:?}", server_path);
+
+            let child = std::process::Command::new(&server_path)
+                .current_dir(server_path.parent().unwrap())
+                .spawn()
+                .expect("no se pudo lanzar el servidor Python");
+
+            *app.state::<ServerProcess>().0.lock().unwrap() = Some(child);
+            Ok(())
+        })
+        .on_window_event(|window, event| {
+            if let tauri::WindowEvent::Destroyed = event {
+                let state = window.state::<ServerProcess>();
+                let child = state.0.lock().unwrap().take();
+                if let Some(mut process) = child {
+                    let _ = process.kill();
+                    println!("[SERVER] Proceso terminado");
+                };
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/backend/server.spec
@@ -0,0 +1,86 @@
+# src/backend/server.spec
+from PyInstaller.utils.hooks import collect_all
+
+block_cipher = None
+
+mediapipe_datas, mediapipe_binaries, mediapipe_hiddenimports = collect_all("mediapipe")
+cv2_datas, cv2_binaries, cv2_hiddenimports = collect_all("cv2")
+mpl_datas, mpl_binaries, mpl_hiddenimports = collect_all("matplotlib")
+sklearn_datas, sklearn_binaries, sklearn_hiddenimports = collect_all("sklearn")
+
+a = Analysis(
+    ["server.py"],
+    pathex=["."],
+    binaries=cv2_binaries + mediapipe_binaries + mpl_binaries + sklearn_binaries,
+    datas=[
+        ("models/hand_state_rf.pkl", "models"),
+        *mediapipe_datas,
+        *cv2_datas,
+        *mpl_datas,
+        *sklearn_datas,
+    ],
+    hiddenimports=[
+        "gestures",
+        "gestures.base",
+        "gestures.scroll",
+        "gestures.volume",
+        "gestures.zoom",
+        "gestures.screenshot",
+        "gestures.close_window",
+        "gestures.pause",
+        "gestures.mute",
+        "gestures.task_view",
+        "vision",
+        "vision.camera",
+        "vision.hand_tracker",
+        "vision.state_classifier",
+        "pipeline",
+        "pipeline.cooldown_manager",
+        "pipeline.gesture_manager",
+        "pipeline.state_stabilizer",
+        "utils",
+        "utils.geometry",
+        "joblib",
+        "pandas",
+        "pynput",
+        "pynput.keyboard",
+        "websockets",
+        "websockets.legacy",
+        "websockets.legacy.server",
+        "matplotlib",
+        "matplotlib.pyplot",
+        "matplotlib.backends.backend_agg",
+        *mediapipe_hiddenimports,
+        *cv2_hiddenimports,
+        *mpl_hiddenimports,
+        *sklearn_hiddenimports,
+    ],
+    hookspath=[],
+    runtime_hooks=[],
+    excludes=["tkinter", "_tkinter", "tk", "tcl"],
+    cipher=block_cipher,
+)
+
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    [],
+    exclude_binaries=True,
+    name="server",
+    debug=False,
+    strip=False,
+    upx=False,
+    console=True,
+)
+
+coll = COLLECT(
+    exe,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
+    strip=False,
+    upx=False,
+    name="server",
+)
